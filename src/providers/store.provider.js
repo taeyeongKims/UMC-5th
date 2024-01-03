c858bda8d328a9c216b1280417b69a905b8ae04d
taeyoungkims <xodud8530@gmail.com>
2024-01-03 17:44:55 +0900

    store.provider update

--- src/providers/store.provider.js
+++ src/providers/store.provider.js
@@ -3,6 +3,7 @@ import { getPreviewReview} from "./../models/store.dao.js";
 
 
 export const getReview = async (storeId, query) => {
-    const {reviewId, size = 3} = query;
+    const reviewId  = query.reviewId;
+    const size = query.paging !== undefined ? query.paging : 3;
     return previewReviewResponseDTO(await getPreviewReview(reviewId, size, storeId));
 }
